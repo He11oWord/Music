a3455472d477c37135732ec407ffa2cd3776453e
lizhao <lizhaoandroid@163.com>
2015-11-05 20:23:44 +0800

    first-11.5 commit

---
+++ src/com/pianostudy/btn_touch/ButtonWhite1.java
@@ -0,0 +1,21 @@
+package com.pianostudy.btn_touch;
+
+
+import android.media.SoundPool;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import java.util.HashMap;
+
+import com.pianostudy.ButtonActivity;
+
+class MainActivity$1
+  implements View.OnTouchListener
+{
+  public boolean onTouch(View paramView, MotionEvent paramMotionEvent)
+  {
+    if (paramMotionEvent.getAction() == 0){}//°´ÏÂ
+    	//soundPool.play(((Integer)has.get(Integer.valueOf(1))).intValue(), 1.0F, 1.0F, 1, 0, 1.0F);
+    return false;
+  }
+}
